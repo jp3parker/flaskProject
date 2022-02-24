e046ff50cc0abf78ab4103a399e0ff9b5e54246c
jhp59 <jhp59@txstate.edu>
2022-02-24 14:41:32 -0600

    Added some stuff...seeing if git commit will make run better.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def page_example():  # put application's code here
+    return render_template("Intro.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    render_template("Intro.html")
